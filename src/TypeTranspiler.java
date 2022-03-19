dd5e123e48e7518056dd6e8eac1f64df3f120c76
christopher.laforet <christopher.laforet@silverrailtech.com>
2022-03-19 13:50:00 -0400

    Initial state mostly solved solution

---
+++ src/TypeTranspiler.java
@@ -0,0 +1,11 @@
+public class TypeTranspiler {
+    
+    public TypeTranspiler(String s) {
+        // Your code here!
+    }
+    
+    public String transpile() {
+        // Your code here too!
+        return null;
+    }
+}
